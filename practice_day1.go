81a7891a9bf21e9dbbbff2b93d8d481b35bdf85d
kvochatik <kvochatik@yandex.ru>
2025-04-19 22:53:40 +0500

    меняю первую практику

--- practice_day1.go
+++ practice_day1.go
@@ -3,7 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(chet(4))
+	fmt.Println(chet(4))//8
+	defer fmt.Println(chet(6))// 12
+	defer fmt.Println(chet(2))// 4
 }
 
 func chet (sum int) int {
